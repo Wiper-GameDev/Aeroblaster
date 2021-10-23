5f5e5a569ed8a250867a1f06f8c5c1a3af1c546d
Wiper-R <rshivang12345@gmail.com>
2021-10-23 17:14:37 +0530

    Player Animation Working

--- src/Actors/Player/Player.gd
+++ src/Actors/Player/Player.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 
 ### Variables
 # Export
-export var speed := Vector2(300, 1000)
-export var gravity := 4000.0
+export var speed := Vector2(300, 650)
+export var gravity := 2000.0
 export var MAX_JUMPS := 2
 
 # Process
@@ -21,32 +21,76 @@ func _ready() -> void:
 	$AnimatedSprite.play("idle")
 
 func _physics_process(delta: float) -> void:
+	# Reset Remaining Jumps
+	if is_on_floor():
+		remaining_jumps = MAX_JUMPS
+		
+	# Getting Player Direction
 	var direction := get_direction()
-	var is_jump_interrupted := Input.is_action_just_released("jump") and _velocity.y < 0.0
 	_velocity = calculate_move_velocity(
 		_velocity,
 		direction,
-		speed,
-		is_jump_interrupted
+		speed
 		)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	# Setting player flip
+	set_flip(direction)
+	
+	# Changing Animations
+	change_animation(direction)
+	
 	
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1 if (Input.is_action_just_pressed("jump") and is_on_floor() and remaining_jumps > 0) else 1
+		-1 if (Input.is_action_just_pressed("jump") and remaining_jumps > 0) else 1
 	)
+	
+func set_flip(direction: Vector2) -> void:
+	"""
+	Set sprite flip according to direction
+	"""
+	$AnimatedSprite.flip_h = $AnimatedSprite.flip_h if direction.x == 0 else direction.x < 0
+	
+	
+func set_animation(anim: String) -> void:
+	if $AnimatedSprite.animation == anim:
+		return
+		
+	$AnimatedSprite.play(anim)
+	
+func change_animation(direction: Vector2) -> void:
+	if is_on_floor():
+		if abs(direction.x):
+			set_animation("run")
+		else:
+			set_animation("idle")
+	else:
+		if _velocity.y > 0:
+			set_animation("fall")
+		elif _velocity.y < 0:
+			set_animation("jump")
 
 func calculate_move_velocity(
 	linear_velocity: Vector2,
 	direction: Vector2,
-	speed: Vector2,
-	is_jump_interrupted : bool
+	speed: Vector2
 ) -> Vector2:
 	"""
 	Returns player move velocity
 	Handles: Jump, Movement (Left and Right)
 	"""
 	var out := linear_velocity
-	return Vector2.ZERO
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	
+	if direction.y == -1.0:
+		# Decreasing Remaining Jumps
+		remaining_jumps -= 1
+	
+		out.y = speed.y * direction.y
+
+		
+	return out
